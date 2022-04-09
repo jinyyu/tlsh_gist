cf107a98a48d168375e79803fbb30df02fe45655
lijunyun <lijunyun@cmcm.com>
2022-04-09 11:05:53 +0800

    just save

--- tlsh_gist.h
+++ tlsh_gist.h
@@ -1,7 +1,11 @@
-extern "C" {
+extern "C"
+{
 #include "postgres.h"
 #include "fmgr.h"
 }
 
+#define TLSH_HASH_LENGTH 70
+#define TLSH_INTERNAL_LENGTH 35
 
-
+extern void from_hex(const char *psrc, int len, unsigned char *pdest);
+extern void to_hex(unsigned char *psrc, int len, char *pdest);
--- tlsh_type.cpp
+++ tlsh_type.cpp
@@ -1,25 +1,89 @@
 #include "tlsh_gist.h"
 #include <tlsh/tlsh.h>
 
-extern "C" {
+extern "C"
+{
 
-PG_FUNCTION_INFO_V1(tlsh_in);
-PG_FUNCTION_INFO_V1(tlsh_out);
+	PG_FUNCTION_INFO_V1(tlsh_in);
+	PG_FUNCTION_INFO_V1(tlsh_out);
+}
 
+void to_hex(unsigned char *psrc, int len, char *pdest)
+{
+	static unsigned char HexLookup[513] = {
+		"000102030405060708090A0B0C0D0E0F"
+		"101112131415161718191A1B1C1D1E1F"
+		"202122232425262728292A2B2C2D2E2F"
+		"303132333435363738393A3B3C3D3E3F"
+		"404142434445464748494A4B4C4D4E4F"
+		"505152535455565758595A5B5C5D5E5F"
+		"606162636465666768696A6B6C6D6E6F"
+		"707172737475767778797A7B7C7D7E7F"
+		"808182838485868788898A8B8C8D8E8F"
+		"909192939495969798999A9B9C9D9E9F"
+		"A0A1A2A3A4A5A6A7A8A9AAABACADAEAF"
+		"B0B1B2B3B4B5B6B7B8B9BABBBCBDBEBF"
+		"C0C1C2C3C4C5C6C7C8C9CACBCCCDCECF"
+		"D0D1D2D3D4D5D6D7D8D9DADBDCDDDEDF"
+		"E0E1E2E3E4E5E6E7E8E9EAEBECEDEEEF"
+		"F0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF"};
+	unsigned short *pwHex = (unsigned short *)HexLookup;
+	unsigned short *pwDest = (unsigned short *)pdest;
+
+	for (int i = 0; i < len; i++)
+	{
+		*pwDest = pwHex[*psrc];
+		pwDest++;
+		psrc++;
+	}
+	*((unsigned char *)pwDest) = 0; // terminate the string
+}
+
+void from_hex(const char *psrc, int len, unsigned char *pdest)
+{
+	static unsigned char DecLookup[] = {
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // gap before first hex digit
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9,		   // 0123456789
+		0, 0, 0, 0, 0, 0, 0,				   // :;<=>?@ (gap)
+		10, 11, 12, 13, 14, 15,				   // ABCDEF
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // GHIJKLMNOPQRS (gap)
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // TUVWXYZ[/]^_` (gap)
+		10, 11, 12, 13, 14, 15				   // abcdef
+	};
+
+	for (int i = 0; i < len; i += 2)
+	{
+		unsigned d = DecLookup[*(unsigned char *)(psrc + i)] << 4;
+		d |= DecLookup[*(unsigned char *)(psrc + i + 1)];
+		*pdest++ = d;
+	}
 }
 
 
-Datum
-tlsh_in(PG_FUNCTION_ARGS)
-{
-    char *str = PG_GETARG_CSTRING(0);
-    Tlsh tlsh;
-    int ret = tlsh.fromTlshStr(str);
-    if (ret != 0 || tlsh.isValid()) {
-           elog(ERROR, "invalid tlsh hash");
-    }
 
-    char * buffer = (char *) palloc( TLSH_STRING_BUFFER_LEN );
-    tlsh.getHash(buffer, TLSH_STRING_BUFFER_LEN);
-    PG_RETURN_POINTER(buffer);
+Datum tlsh_in(PG_FUNCTION_ARGS)
+{
+	char *str = PG_GETARG_CSTRING(0);
+	if (!str ||  strlen(str) != TLSH_HASH_LENGTH) {
+		elog(ERROR, "null tlash");
+	}
+	Tlsh tlsh;
+	tlsh.
+
+	unsigned char* buffer = (unsigned char*) palloc(TLSH_INTERNAL_LENGTH);
+	from_hex(str, TLSH_INTERNAL_LENGTH, buffer);
+
+	PG_RETURN_POINTER(buffer);
+}
+
+Datum tlsh_out(PG_FUNCTION_ARGS)
+{
+	unsigned char *buffer = PG_GETARG_POINTER(0);
+	char *str = palloc(TLSH_HASH_LENGTH + 1);
+	to_hex(buffer, TLSH_INTERNAL_LENGTH, str);
+	str[TLSH_HASH_LENGTH] = 0;
+
+	PG_RETURN_CSTRING(buffer);
 }
