02026e99c292701e77aa10f33883ccac762aaf8f
lijunyun <jinyyu@hotmail.com>
2022-04-11 09:35:52 +0800

    remove log

--- tlsh_gist.cpp
+++ tlsh_gist.cpp
@@ -17,17 +17,8 @@ extern "C"
     PG_FUNCTION_INFO_V1(tlsh_same);
 }
 
-static void construct_tlsh(Tlsh &tlsh, unsigned char *data)
-{
-    char hex_str[TLSH_HASH_LENGTH + 1];
-    to_hex(data, TLSH_INTERNAL_LENGTH, hex_str);
-    hex_str[TLSH_HASH_LENGTH] = 0;
-    tlsh.fromTlshStr(hex_str);
-    if (!tlsh.isValid())
-    {
-        elog(ERROR, "invalid tlsh data");
-    }
-}
+static Datum copy_tlsh(Datum src);
+static void construct_tlsh(Tlsh &tlsh, unsigned char *data);
 
 Datum tlsh_dist(PG_FUNCTION_ARGS)
 {
@@ -46,7 +37,6 @@ Datum tlsh_dist(PG_FUNCTION_ARGS)
 
 Datum tlsh_consistent(PG_FUNCTION_ARGS)
 {
-    elog(INFO, "tlsh_consistent");
     GISTENTRY *entry = (GISTENTRY *)PG_GETARG_POINTER(0);
     Datum query = PG_GETARG_DATUM(1);
     StrategyNumber strategy = (StrategyNumber)PG_GETARG_UINT16(2);
@@ -70,13 +60,59 @@ Datum tlsh_consistent(PG_FUNCTION_ARGS)
 
 Datum tlsh_union(PG_FUNCTION_ARGS)
 {
-    elog(ERROR, "tlsh_union not impl");
+    GistEntryVector *entryvec = (GistEntryVector *)PG_GETARG_POINTER(0);
+    GISTENTRY *enties = entryvec->vector;
+    int numranges = entryvec->n;
+
+    if (numranges <= 2)
+    {
+        PG_RETURN_POINTER(copy_tlsh(enties[0].key));
+    }
+
+    int32 dist_table[numranges + 1][numranges + 1];
+    memset(dist_table, 0, sizeof(dist_table));
+
+    for (int i = 1; i < numranges; i++)
+    {
+        for (int j = i + 1; j < numranges; j++)
+        {
+            Datum datum_i = enties[i].key;
+            Datum datum_j = enties[j].key;
+            int32 dist = DatumGetInt32(DirectFunctionCall2(tlsh_dist, datum_i, datum_j));
+            dist_table[i][j] = dist;
+            dist_table[j][i] = dist;
+        }
+    }
+
+    int32 sum_table[numranges + 1];
+    memset(sum_table, 0, sizeof(sum_table));
+
+    for (int i = 1; i < numranges; i++)
+    {
+        for (int j = i + 1; j < numranges; j++)
+        {
+            int32 dist = dist_table[i][j];
+            sum_table[i] += dist;
+        }
+    }
+
+    int min_index = 0;
+    int32 min_dist = sum_table[0];
+    for (int i = 1; i < numranges; i++) 
+    {
+        int32 dist_sum = sum_table[i];
+        if (dist_sum < min_dist)
+        {
+            min_index = i;
+            min_dist = min_dist;
+        }
+    }
+
+    PG_RETURN_POINTER(copy_tlsh(enties[min_index].key));
 }
 
 Datum tlsh_penalty(PG_FUNCTION_ARGS)
 {
-    elog(INFO, "tlsh_penalty");
-
     GISTENTRY *origentry = (GISTENTRY *)PG_GETARG_POINTER(0);
     GISTENTRY *newentry = (GISTENTRY *)PG_GETARG_POINTER(1);
     float *penalty = (float *)PG_GETARG_POINTER(2);
@@ -89,8 +125,6 @@ Datum tlsh_penalty(PG_FUNCTION_ARGS)
 
 Datum tlsh_picksplit(PG_FUNCTION_ARGS)
 {
-    elog(INFO, "tlsh_picksplit");
-
     GistEntryVector *entryvec = (GistEntryVector *)PG_GETARG_POINTER(0);
     GIST_SPLITVEC *v = (GIST_SPLITVEC *)PG_GETARG_POINTER(1);
     OffsetNumber maxoff = entryvec->n - 1; /* Valid items in entryvec->vector[] are indexed 1..maxoff */
@@ -122,10 +156,10 @@ Datum tlsh_picksplit(PG_FUNCTION_ARGS)
     }
 
     v->spl_left = (OffsetNumber *)palloc0(entryvec->n * sizeof(OffsetNumber));
-    v->spl_ldatum = entryvec->vector[left_number].key;
+    v->spl_ldatum = copy_tlsh(entryvec->vector[left_number].key);
 
     v->spl_right = (OffsetNumber *)palloc0(entryvec->n * sizeof(OffsetNumber));
-    v->spl_rdatum = entryvec->vector[right_number].key;
+    v->spl_rdatum = copy_tlsh(entryvec->vector[right_number].key);
 
     for (OffsetNumber i = FirstOffsetNumber; i <= maxoff; i = OffsetNumberNext(i))
     {
@@ -147,8 +181,6 @@ Datum tlsh_picksplit(PG_FUNCTION_ARGS)
 
 Datum tlsh_same(PG_FUNCTION_ARGS)
 {
-    elog(INFO, "tlsh_same");
-
     Datum d1 = ObjectIdGetDatum(PG_GETARG_POINTER(0));
     Datum d2 = ObjectIdGetDatum(PG_GETARG_POINTER(1));
     bool *result = (bool *)PG_GETARG_POINTER(2);
@@ -156,4 +188,23 @@ Datum tlsh_same(PG_FUNCTION_ARGS)
     int ret = memcmp((void *)d1, (void *)d2, TLSH_INTERNAL_LENGTH);
     *result = (ret == 0 ? true : false);
     PG_RETURN_POINTER(result);
+}
+
+Datum copy_tlsh(Datum src)
+{
+    Datum dst = (Datum)palloc(TLSH_INTERNAL_LENGTH);
+    memcpy((void *)dst, (void *)src, TLSH_INTERNAL_LENGTH);
+    return dst;
+}
+
+void construct_tlsh(Tlsh &tlsh, unsigned char *data)
+{
+    char hex_str[TLSH_HASH_LENGTH + 1];
+    to_hex(data, TLSH_INTERNAL_LENGTH, hex_str);
+    hex_str[TLSH_HASH_LENGTH] = 0;
+    tlsh.fromTlshStr(hex_str);
+    if (!tlsh.isValid())
+    {
+        elog(ERROR, "invalid tlsh data");
+    }
 }
